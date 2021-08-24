65caffc9908d72d98006916ea6045b7cbffb02bb
Celio Grand <celiogrand@outlook.com>
2021-08-24 11:39:37 +0800

    add panel prop log base

--- tofon/panel.py
+++ tofon/panel.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy.types import Panel
-from bpy.props import BoolVectorProperty
+from bpy.props import BoolVectorProperty, FloatProperty
 
 class TOFON_PT_para_setter(Panel):
     bl_space_type = 'VIEW_3D'
@@ -19,12 +19,19 @@ class TOFON_PT_para_setter(Panel):
         col = row.column()
         col.prop(scene, 'ToF_mode', index=2, text='B')
         row = layout.row()
+        row.prop(scene, 'ToF_base', emboss=False, slider=True)
+        row = layout.row()
         row.operator('scene.apply_tof_mode')
 
 def register():
     bpy.utils.register_class(TOFON_PT_para_setter)
-    bpy.types.Scene.ToF_mode = BoolVectorProperty(name='Mode', size=3)
+    bpy.types.Scene.ToF_mode = BoolVectorProperty(name='Mode')
+    bpy.types.Scene.ToF_base = FloatProperty(
+        name='Logarithmic Base', precision=3,
+        default=0.950, min=0.001, max=0.999
+        )
 
 def unregister():
     del bpy.types.Scene.ToF_mode
+    del bpy.types.Scene.ToF_base
     bpy.utils.unregister_class(TOFON_PT_para_setter)
